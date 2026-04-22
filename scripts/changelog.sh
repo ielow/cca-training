fecbc672b62164e18443d77c96620f52659be9c3
ielow <pako.adn@gmail.com>
2026-04-22 00:18:08 -0600

    Update paths

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-NODE_VERSION=$(node -p -e "require('package.json').version")
+NODE_VERSION=$(node -p -e "require('../package.json').version")
 
 if [ "$1" = "stdout" ]; then
-    npm exec git-cliff -o - --unreleased -c 'scripts/github.toml' --tag $NODE_VERSION
+    npm exec git-cliff -o - --unreleased -c './github.toml' --tag $NODE_VERSION
 else
-    npm exec git-cliff -o -c './github.toml' 'CHANGELOG.md' --tag $NODE_VERSION
+    npm exec git-cliff -o -c './github.toml' '../CHANGELOG.md' --tag $NODE_VERSION
 fi
